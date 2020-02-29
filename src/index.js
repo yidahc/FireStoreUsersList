e7c52da31adaefee3834524876e6c3544686355a
yidahc <yidah.a.curiel@gmail.com>
2020-02-28 23:48:45 -0600

    connecting to firestore in real-time and rendering first and last names from users collections

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import FireStoreProvider from './Components/utils/firestore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <FireStoreProvider> {/* wrapeamos el app en el proveedor del contexto de nuestra firestore db */}
+        <App /> 
+    </FireStoreProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
