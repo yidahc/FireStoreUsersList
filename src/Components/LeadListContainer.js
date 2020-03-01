5025a7f5bec684b49948ca248901b00b400afd4b
yidahc <yidah.a.curiel@gmail.com>
2020-02-29 20:07:12 -0600

    agregando funcionalidad para agregar leads en LeadListContainer. Falta la funcionalidad para editar los leads

--- src/Components/LeadListContainer.js
+++ src/Components/LeadListContainer.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { List, ListItem, ListItemText, Collapse } from '@material-ui/core';
-import NestedList from './NestedList';
+import SelectedLead from './SelectedLead';
 import NewLead from './NewLead';
+import EditLead from './NewLead';
+import { FirebaseContext } from '../Components/utils/firebase' 
+// importamos el contexto de nuestra instancia de firebase para poder usar esa app con su contexto aqui
+import 'firebase/firestore'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -21,11 +25,19 @@ const useStyles = makeStyles(theme => ({
 
 function MainList({ list }) {
   const classes = useStyles();
-  const [selectedID, setSelected] = React.useState(null);
+  const [selectedID, setSelected] = useState(null);
+  const [edit, setEdit] = useState(false);
+  const firebaseApp = useContext(FirebaseContext) 
 
   const handleClick = (id) => {
+    setEdit(false); // para que nos muestre los datos del lead en vez del form para editarlo (si es que estabamos editando otro lead)
     setSelected(id) //guardamos el id del 'lead' seleccionado en el 'selectedID' state
   };
+
+  const addLead = (params) => {
+    console.log(params)
+    firebaseApp.firestore().collection('users').add(params)
+  }
 
   return (
     <div className={classes.root}>
@@ -35,13 +47,16 @@ function MainList({ list }) {
             <ListItemText primary={`${e.name} ${e.last_name}`} />
           </ListItem>
         ))}
-        <NewLead />
+        <NewLead addLead={addLead} />
       </List>
       <Collapse className={classes.collapse} in={true} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
-          {selectedID ?
-            <NestedList leadID={selectedID} />
-            : null}
+          {selectedID && !edit ?
+            <SelectedLead leadID={selectedID} setEdit={setEdit}/>
+            : selectedID && edit ?
+               <EditLead leadID={selectedID}/>
+               : null
+          }
         </List>
       </Collapse>
     </div>
